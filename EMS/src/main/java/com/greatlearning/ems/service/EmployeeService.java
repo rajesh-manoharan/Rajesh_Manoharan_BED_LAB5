ecc047b52009af7879b065653a9debdd1486b2c0
rajesh-manoharan <111693523+rajesh-manoharan@users.noreply.github.com>
2023-01-14 11:49:06 +0530

    Add files via upload

---
+++ EMS/src/main/java/com/greatlearning/ems/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.greatlearning.ems.service;
+
+import java.util.List;
+
+import com.greatlearning.ems.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> listEmployees();
+	
+	Employee saveEmployee(Employee employee);
+	
+	// Update - Start
+	Employee getEmployeeById(int id);
+	Employee updateEmployee(Employee employee);
+	// Update - End
+	
+	void deleteById(int id);
+}
